e9af5599c92aa460d08dffa4f468a5ba1b193d26
Darsshan Nair <tysonjasmin@gmail.com>
2026-05-01 18:23:45 +0800

    feat: register ITodoRepository and TodoCubit in DI

--- lib/core/di/di_container.dart
+++ lib/core/di/di_container.dart
@@ -1,17 +1,30 @@
 import 'package:get_it/get_it.dart';
 import '../services/i_model_service.dart';
+import '../services/i_todo_repository.dart';
 import '../services/model_service.dart';
+import '../services/todo_repository.dart';
+import '../../presentation/bloc/cubits/todo_cubit.dart';
 
 final GetIt serviceLocator = GetIt.instance;
 
 class ServiceLocator {
   static Future<void> init() async {
     await initServices();
+    initBlocs();
   }
 
   static Future<void> initServices() async {
     serviceLocator.registerLazySingleton<IModelService>(
       () => ModelService(),
+    );
+    serviceLocator.registerLazySingleton<ITodoRepository>(
+      () => TodoRepository(),
+    );
+  }
+
+  static void initBlocs() {
+    serviceLocator.registerFactory<TodoCubit>(
+      () => TodoCubit(serviceLocator<ITodoRepository>()),
     );
   }
 }
