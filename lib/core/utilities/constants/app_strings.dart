2f8f0878ec67f874a564905795e7a923769b7eb3
Darsshan Nair <tysonjasmin@gmail.com>
2026-05-01 18:26:16 +0800

    feat: add todo-related string constants

---
+++ lib/core/utilities/constants/app_strings.dart
@@ -0,0 +1,15 @@
+class AppStrings {
+  static const String todosTitle = 'Todos';
+  static const String newTask = 'New Task';
+  static const String editTask = 'Edit Task';
+  static const String cancel = 'Cancel';
+  static const String add = 'Add';
+  static const String save = 'Save';
+  static const String chatTab = 'Chat';
+  static const String todosTab = 'Todos';
+  static const String emptyTodos = 'No tasks yet. Tap + to add one.';
+  static const String typeTask = 'Type a task...';
+  static const String editHint = 'Edit task...';
+  static const String somethingWentWrong = 'Something went wrong.';
+  static const String retry = 'Retry';
+}
